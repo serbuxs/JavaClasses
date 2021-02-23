50e93019d0493802cf0f1422f6030a8fdd89d936
Siarhei Bukhavets <sbukhovets@gmail.com>
2021-02-23 23:29:50 +0300

    Added printAL and main methods.

--- src/src/ArrayListMagnet.java
+++ src/src/ArrayListMagnet.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class ArrayListMagnet {
 
+
+    public static void printAL(ArrayList<String> al) {
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        a.add(0, "ноль");
+        a.add(1, "один");
+        a.add(2, "два");
+        a.add(3, "три");
+    }
 }
